18f2dee12123c746c958099bd3d8de1b387857e0
faurwalker <47984939+faurwalker@users.noreply.github.com>
2021-04-30 21:00:28 +0300

    setup of the express js server

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+console.log("Hello my world!")
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+app.get('/status', (req,res) =>{
+    res.send({
+        message: "hacker news"
+    })
+})
+app.listen(process.env.PORT || 8081)
